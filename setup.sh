ea7922ef15f4b643f972691649b3dde5198ab712
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-27 19:30:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,16 @@ sudo service nginx restart
 cd ~/Plant-Disease-Detection
 # Install FastAPI application requirements
 pip3 install -r aws.txt
+pip3 install tensorflow --no-cache-dir
 python3 download.py
 # Kill any service running on port 80
 sudo kill -9 $(sudo lsof -t -i:80)
 sudo service nginx restart
 # Run the application with nohup so the application runs as a background process
-nohup python3 -m uvicorn app:app --reload --host 0.0.0.0
+sudo fallocate -l 3G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo free -h
+cat /proc/sys/vm/swappiness
+nohup uvicorn app:app --reload --host 0.0.0.0
